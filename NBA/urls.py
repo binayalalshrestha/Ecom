b1cee1c0ed064a9c55a782389e4844bd41263719
clumszy <binaya.shrestha08@gmail.com>
2023-04-11 09:53:44 +0545

    api

--- NBA/urls.py
+++ NBA/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path, include
-from . import views
+from django.urls import path
+from .views import (
+    PlayerList,
+    PlayerDetail,
+    BasketballPlayerList,
+    BasketballPlayerDetail,
+    NBAPlayerList,
+    NBAPlayerDetail,
+)
 
 urlpatterns = [
-    
+    path('players/', PlayerList.as_view(), name='player-list'),
+    path('players/<int:pk>/', PlayerDetail.as_view(), name='player-detail'),
+    path('basketball-players/', BasketballPlayerList.as_view(), name='basketballplayer-list'),
+    path('basketball-players/<int:pk>/', BasketballPlayerDetail.as_view(), name='basketballplayer-detail'),
+    path('nba-players/', NBAPlayerList.as_view(), name='nbaplayer-list'),
+    path('nba-players/<int:pk>/', NBAPlayerDetail.as_view(), name='nbaplayer-detail'),
 ]
--- NBA/views.py
+++ NBA/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Player
+from .serializers import PlayerSerializer, BasketballPlayerSerializer, NBAPlayerSerializer
 
-# Create your views here.
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class BasketballPlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = BasketballPlayerSerializer
+
+class BasketballPlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = BasketballPlayerSerializer
+
+class NBAPlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = NBAPlayerSerializer
+
+class NBAPlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = NBAPlayerSerializer
